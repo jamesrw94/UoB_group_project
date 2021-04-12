c7647031521550a45554c13d82a6bafc1f088ba5
josehed <55767257+josehed@users.noreply.github.com>
2021-04-12 11:54:41 +0100

    added template for drag and drop

--- angular/src/app/topicpage/brexit/brexit.component.ts
+++ angular/src/app/topicpage/brexit/brexit.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-brexit',
   templateUrl: './brexit.component.html',
@@ -7,9 +6,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BrexitComponent implements OnInit {
   brexitPageUrl = '../../assets/brexit.jpeg';
+  theSunUrl = '../../assets/theSun.png';
+  theGuardianUrl = '../../assets/theGuardian.png';
+  
   constructor() { }
-
+  
   ngOnInit(): void {
-  }
 
+  }
 }
