498b84c3bca956cafe74f22f0291fcc80279b00f
josehed <55767257+josehed@users.noreply.github.com>
2021-04-13 12:05:03 +0100

    changing back 404 page as i used that do demo dragging

--- angular/src/app/page-not-found/page-not-found.component.ts
+++ angular/src/app/page-not-found/page-not-found.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-page-not-found',
@@ -7,28 +6,7 @@ import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag
   styleUrls: ['./page-not-found.component.css']
 })
 export class PageNotFoundComponent implements OnInit {
-  theSunUrl = '../../assets/theSun.png';
-  todo = [
-  ];
-
-  done = [
-    this.theSunUrl,
-  ];
-
-  next = [
-
-  ];
-
-  drop(event: CdkDragDrop<string[]>) {
-    if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-    } else {
-      transferArrayItem(event.previousContainer.data,
-                        event.container.data,
-                        event.previousIndex,
-                        event.currentIndex);
-    }
-  }
+  
   constructor() { }
 
   ngOnInit(): void {
